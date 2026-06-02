9006d2eae2d809cd6a5a6d474c326ae1b63794a2
ghazikouissah <ghazikhouissah@gmail.com>
2026-06-02 03:14:10 +0100

    dashboard: ProtectedRoute, map, security

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -3,6 +3,7 @@ import { AuthProvider } from "./context/AuthContext";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import CamionDetail from "./pages/CamionDetail";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 
 function App() {
@@ -11,8 +12,8 @@ function App() {
             <BrowserRouter>
                 <Routes>
                   <Route path="/" element={<Login />} />
-                  <Route path="/dashboard" element={<Dashboard />} />
-                  <Route path="/camions/:id" element={<CamionDetail />} />
+                  <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+                  <Route path="/camions/:id" element={<ProtectedRoute><CamionDetail /></ProtectedRoute>} />
                   </Routes>
             </BrowserRouter>
         </AuthProvider>
